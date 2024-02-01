5435d3cc18db062920fdca3b8a42e30088e7a84b
任兵长 <1362206371@qq.com>
2024-02-01 21:17:15 +0800

    2024—01—30 每日一题

---
+++ R02_LeetCode/每日一题/2024年02月/R02_01_LCP24_数字游戏.go
@@ -0,0 +1,23 @@
+package main
+
+import "container/heap"
+
+func numsGame(nums []int) []int {
+	const mod = 1_000_000_007
+	ans := make([]int, len(nums))
+	left := hp{}  // 维护较小的一半，大根堆（小根堆取负号）
+	right := hp{} // 维护较大的一半，小根堆
+	for i, b := range nums {
+		b -= i
+		if i%2 == 0 {
+			heap.Push(&right, -left.pushPop(-b))
+			x := right.IntSlice[0] // 中位数
+			// 原本要减去 left.sum，但由于 left 所有元素都取负号，所以负负得正改为加法
+			ans[i] = (right.sum - x + left.sum) % mod
+		} else {
+			heap.Push(&left, -right.pushPop(b))
+			ans[i] = (right.sum + left.sum) % mod
+		}
+	}
+	return ans
+}

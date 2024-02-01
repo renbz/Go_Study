275a93f8aa7f612689cc8b765de635db223ce093
任兵长 <1362206371@qq.com>
2024-02-01 21:14:31 +0800

    2024—01—29 每日一题

--- R02_LeetCode/每日一题/2024年01月/R01_31_2670_找出不同元素数目差数组.go
+++ R02_LeetCode/每日一题/2024年01月/R01_31_2670_找出不同元素数目差数组.go
@@ -1,1 +1,17 @@
 package main
+
+func distinctDifferenceArray(nums []int) []int {
+	st := map[int]struct{}{}
+	sufCnt := make([]int, len(nums)+1)
+	for i := len(nums) - 1; i > 0; i-- {
+		st[nums[i]] = struct{}{}
+		sufCnt[i] = len(st)
+	}
+	var res []int
+	st = map[int]struct{}{}
+	for i := 0; i < len(nums); i++ {
+		st[nums[i]] = struct{}{}
+		res = append(res, len(st)-sufCnt[i+1])
+	}
+	return res
+}

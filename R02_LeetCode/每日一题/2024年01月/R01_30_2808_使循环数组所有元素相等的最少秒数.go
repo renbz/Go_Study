e25ed6cfad146904ecb588a368a4bf11fb6d582a
任兵长 <1362206371@qq.com>
2024-02-01 21:12:31 +0800

    2024—01—27 每日一题

--- R02_LeetCode/每日一题/2024年01月/R01_30_2808_使循环数组所有元素相等的最少秒数.go
+++ R02_LeetCode/每日一题/2024年01月/R01_30_2808_使循环数组所有元素相等的最少秒数.go
@@ -1,1 +1,19 @@
 package main
+
+func minimumSeconds(nums []int) int {
+	pos := map[int][]int{}
+	for i, x := range nums {
+		pos[x] = append(pos[x], i)
+	}
+
+	n := len(nums)
+	ans := n
+	for _, a := range pos {
+		mx := n - a[len(a)-1] + a[0]
+		for i := 1; i < len(a); i++ {
+			mx = max(mx, a[i]-a[i-1])
+		}
+		ans = min(ans, mx)
+	}
+	return ans / 2 // 最后再除 2
+}

2f2794b3e093285a421ff7104846f4c26cbe52e7
任兵长 <1362206371@qq.com>
2024-02-03 22:31:17 +0800

    2024-02-03 每日一题

---
+++ R02_LeetCode/每日一题/2024年02月/R02_03_1690_石子游戏VII.go
@@ -0,0 +1,16 @@
+package main
+
+func stoneGameVII(stones []int) int {
+	n := len(stones)
+	s := make([]int, n+1)
+	for i, x := range stones {
+		s[i+1] = s[i] + x
+	}
+	f := make([]int, n)
+	for i := n - 2; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			f[j] = max(s[j+1]-s[i+1]-f[j], s[j]-s[i]-f[j-1])
+		}
+	}
+	return f[n-1]
+}
